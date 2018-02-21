b6a3c4aebcb7eea111a931cce755bd74923b5794
Ravgus <Alex1996saruman@yandex.ru>
2018-02-21 14:06:22 +0200

    Добавлено category (views,routes,models,controllers, migrations)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function(){
     Route::get('/', ['uses' => 'DashboardController@dashboard', 'as' => 'admin.index']);
+    Route::resource('/category', 'CategoryController', ['as' => 'admin']);
 });
 
 Route::get('/', function () {
